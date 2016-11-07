f5cf4a22649ac872ac4c2fd13c3e836d1a4c8898
Ray Zane <ray@promptworks.com>
2016-11-06 19:47:29 -0500

    Use the TestSearchRepo instead of creating another test module

--- test/elastic_sync/repo_test.exs
+++ test/elastic_sync/repo_test.exs
@@ -1,7 +1,10 @@
 defmodule ElasticSync.RepoTest do
   use ExUnit.Case
-  doctest ElasticSync.Repo
   import Tirexs.HTTP
+
+  alias ElasticSync.TestSearchRepo
+
+  doctest ElasticSync.Repo
 
   defmodule Thing do
     defstruct [:id, :name]
@@ -10,10 +13,6 @@ defmodule ElasticSync.RepoTest do
     def search_data(record) do
       Map.take(record, [:id, :name])
     end
-  end
-
-  defmodule Repo do
-    use ElasticSync.Repo
   end
 
   defp find(id) do
@@ -27,51 +26,51 @@ defmodule ElasticSync.RepoTest do
   end
 
   test "to_collection_url/1 generates a valid url" do
-    assert Repo.to_collection_url(%Thing{}) == "/elastic_sync_test/things"
+    assert TestSearchRepo.to_collection_url(%Thing{}) == "/elastic_sync_test/things"
   end
 
   test "to_resource_url/2 generates a valid url" do
-    assert Repo.to_resource_url(%Thing{id: 1}) == "/elastic_sync_test/things/1"
+    assert TestSearchRepo.to_resource_url(%Thing{id: 1}) == "/elastic_sync_test/things/1"
   end
 
   test "insert/1" do
-    {:ok, 201, _} = Repo.insert(%Thing{id: 1})
+    {:ok, 201, _} = TestSearchRepo.insert(%Thing{id: 1})
     assert {:ok, 200, _} = find(1)
   end
 
   test "insert!" do
-    Repo.insert!(%Thing{id: 1})
+    TestSearchRepo.insert!(%Thing{id: 1})
     assert {:ok, 200, _} = find(1)
   end
 
   test "update/1" do
-    Repo.insert!(%Thing{id: 1})
-    assert {:ok, 200, _} = Repo.update(%Thing{id: 1, name: "pasta"})
+    TestSearchRepo.insert!(%Thing{id: 1})
+    assert {:ok, 200, _} = TestSearchRepo.update(%Thing{id: 1, name: "pasta"})
     {:ok, 200, %{_source: source}} = find(1)
     assert source == %{id: 1, name: "pasta"}
   end
 
   test "update!/1" do
-    Repo.insert!(%Thing{id: 1})
-    Repo.update!(%Thing{id: 1, name: "pasta"})
+    TestSearchRepo.insert!(%Thing{id: 1})
+    TestSearchRepo.update!(%Thing{id: 1, name: "pasta"})
     {:ok, 200, %{_source: source}} = find(1)
     assert source == %{id: 1, name: "pasta"}
   end
 
   test "delete/1" do
-    Repo.insert!(%Thing{id: 1})
-    assert {:ok, 200, _} = Repo.delete(%Thing{id: 1, name: "pasta"})
+    TestSearchRepo.insert!(%Thing{id: 1})
+    assert {:ok, 200, _} = TestSearchRepo.delete(%Thing{id: 1, name: "pasta"})
     {:error, 404, _} = find(1)
   end
 
   test "delete!/1" do
-    Repo.insert!(%Thing{id: 1})
-    Repo.delete!(%Thing{id: 1, name: "pasta"})
+    TestSearchRepo.insert!(%Thing{id: 1})
+    TestSearchRepo.delete!(%Thing{id: 1, name: "pasta"})
     {:error, 404, _} = find(1)
   end
 
   test "insert_all/1" do
-    Repo.insert_all(Thing, [
+    TestSearchRepo.insert_all(Thing, [
       %Thing{id: 1, name: "meatloaf"},
       %Thing{id: 2, name: "pizza"},
       %Thing{id: 3, name: "sausage"},
