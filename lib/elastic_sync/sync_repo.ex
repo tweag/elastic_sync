def5bbedb2015f2933958a6139724f6df093750b
Ray Zane <ray@promptworks.com>
2016-12-12 22:54:21 -0500

    Disregard status. Getting back :ok should be enough

--- lib/elastic_sync/sync_repo.ex
+++ lib/elastic_sync/sync_repo.ex
@@ -8,42 +8,42 @@ defmodule ElasticSync.SyncRepo do
       def __elastic_sync__(:search), do: @search
 
       def insert(struct_or_changeset, opts \\ []) do
-        sync_one(:insert, 201, struct_or_changeset, opts)
+        sync_one(:insert, struct_or_changeset, opts)
       end
 
       def insert!(struct_or_changeset, opts \\ []) do
-        sync_one!(:insert!, 201, struct_or_changeset, opts)
+        sync_one!(:insert!, struct_or_changeset, opts)
       end
 
       def update(changeset, opts \\ []) do
-        sync_one(:update, 200, changeset, opts)
+        sync_one(:update, changeset, opts)
       end
 
       def update!(changeset, opts \\ []) do
-        sync_one!(:update!, 200, changeset, opts)
+        sync_one!(:update!, changeset, opts)
       end
 
       def delete(struct_or_changeset, opts \\ []) do
-        sync_one(:delete, 200, struct_or_changeset, opts)
+        sync_one(:delete, struct_or_changeset, opts)
       end
 
       def delete!(struct_or_changeset, opts \\ []) do
-        sync_one!(:delete!, 200, struct_or_changeset, opts)
+        sync_one!(:delete!, struct_or_changeset, opts)
       end
 
       def insert_all(schema_or_source, entries, opts \\ []) do
         with {:ok, records} <- @ecto.insert_all(schema_or_source, entries, opts),
-             {:ok, 200, _} <- @search.insert_all(schema_or_source, records),
+             {:ok, _, _} <- @search.insert_all(schema_or_source, records),
              do: {:ok, records}
       end
 
-      defp sync_one(action, status, struct_or_changeset, opts \\ []) do
+      defp sync_one(action, struct_or_changeset, opts \\ []) do
         with {:ok, record} <- apply(@ecto, action, [struct_or_changeset, opts]),
-             {:ok, ^status, response} <- apply(@search, action, [record]),
+             {:ok, _, response} <- apply(@search, action, [record]),
             do: {:ok, record}
       end
 
-      defp sync_one!(action, status, struct_or_changeset, opts \\ []) do
+      defp sync_one!(action, struct_or_changeset, opts \\ []) do
         result = apply(@ecto, action, [struct_or_changeset, opts])
         apply(@search, action, [result])
         result
