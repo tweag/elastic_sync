43104763e62943f59923090d603e29ed693b5417
Ray Zane <ray@promptworks.com>
2017-04-27 15:28:05 -0400

    Refactor Mix.Tasks.Reindex

--- lib/mix/tasks/elastic_sync/reindex.ex
+++ lib/mix/tasks/elastic_sync/reindex.ex
@@ -6,47 +6,35 @@ defmodule Mix.Tasks.ElasticSync.Reindex do
       Mix.Project.compile(args)
     end
 
-    case reindex(args) do
-      {:error, error} ->
-        Mix.raise "Reindex failed, error: #{inspect error}"
-      other ->
-        other
+    {sync_repo, schema, args} = parse!(args)
+    repo = sync_repo.__elastic_sync__(:ecto)
+    ensure_started!(repo, args)
+    sync_repo.reindex(schema)
+    :ok
+  end
+
+  defp ensure_started!(repo, args) do
+    case Mix.Ecto.ensure_started(repo, args) do
+      {:ok, _, _} ->
+        :ok
+      error ->
+        Mix.raise "Failed to start Ecto, error: #{inspect error}."
     end
   end
 
-  def reindex(args) do
-    with {:ok, schema, sync_repo} <- parse_args(args),
-         {:ok, _, _} <- ensure_started(sync_repo, args),
-         :ok <- sync_repo.reindex(schema),
-         do: :ok
+  defp parse!(args) when length(args) < 2 do
+    Mix.raise "Wrong number of arguments."
+  end
+  defp parse!([sync_repo_name, schema_name | args]) do
+    {compile!(sync_repo_name), compile!(schema_name), args}
   end
 
-  defp ensure_started(sync_repo, args) do
-    Mix.Ecto.ensure_started(sync_repo.__elastic_sync__(:ecto), args)
-  end
-
-  defp parse_args(args) when length(args) < 2 do
-    {:error, "Wrong number of arguments."}
-  end
-
-  defp parse_args([sync_repo_name, schema_name | _args]) do
-    with {:ok, schema} <- parse_elastic_sync(schema_name, 0),
-         {:ok, sync_repo} <- parse_elastic_sync(sync_repo_name, 1),
-         do: {:ok, schema, sync_repo}
-  end
-
-  defp parse_elastic_sync(name, arity) do
-    mod = Module.concat([name])
-
-    case Code.ensure_compiled(mod) do
-      {:module, _} ->
-        if function_exported?(mod, :__elastic_sync__, arity) do
-          {:ok, mod}
-        else
-          {:error, "Module #{inspect mod} isn't using elastic_sync."}
-        end
-      {:error, error} ->
-        {:error, "Could not load #{inspect mod}, error: #{inspect error}."}
+  defp compile!(name) do
+    case [name] |> Module.concat() |> Code.ensure_compiled() do
+      {:module, mod} ->
+        mod
+      error ->
+        Mix.raise "Invalid module name '#{name}', error: #{inspect error}."
     end
   end
 end
